c9cf4835795ba4c94a76b738d2bcfdc4c276242d
Koushyar <koushyarheidaribateni@gmail.com>
2024-06-09 00:30:46 +0330

    feat: Build base files

---
+++ src/components/home/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="bg-red-500">
+      <p>Welcome home!</p>
+    </div>
+  );
+}
---
+++ src/layout/index.tsx
@@ -0,0 +1,15 @@
+import React, { ReactNode } from "react";
+import Footer from "./footer";
+import Navbar from "./navbar";
+
+type LayoutProps = { children: ReactNode };
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+}
